baf1131c238a8c780ad6c45803cd650069ac8417
Deepak Choudhary <114693662+Deepak-Choudhary0@users.noreply.github.com>
2023-05-20 02:06:38 +0530

    Update index.js#01

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Deepak Choudhary</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -15,6 +16,13 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+         <div>
+            <h1>Welcome to My Portfolio</h1>
+            <p>Check out my projects:</p>
+            <Link href="/projects">
+              <a>Go to Projects</a>
+            </Link>
+          </div>
       </main>
 
       <Footer />
