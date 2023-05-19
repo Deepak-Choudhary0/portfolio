d21b4929c0c52b31265bec0a7537236f6584621f
Deepak Choudhary <114693662+Deepak-Choudhary0@users.noreply.github.com>
2023-05-20 02:47:48 +0530

    indtialising again

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -11,21 +8,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-         <div>
-            <h1>Welcome to My Portfolio</h1>
-            <p>Check out my projects:</p>
-            <Link href="/projects">
-              <a>Go to Projects</a>
-            </Link>
-          </div>
-      </main>
-
-      <Footer />
     </div>
   )
 }
